8a81b53d909cb253cc529d450b1d53ff1818d148
an0n77 <003anish@gmail.com>
2020-05-12 08:59:09 +0545

    Initialization Commit

---
+++ src/main/java/com/example/petclinicwebapp/PetClinicWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinicwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petclinicwebapp/PetClinicWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petclinicwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
